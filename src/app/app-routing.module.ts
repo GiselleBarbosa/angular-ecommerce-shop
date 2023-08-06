fd033030049b6328718a4fc022eb139e9ceaa4b4
Giselle Barbosa <g88barbosa@gmail.com>
2023-08-05 21:55:14 -0300

    feat: routes created, migrate project to standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './modules/notFoundPage/notFoundPage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./routes.module').then(module => module.homeRoutes),
+  },
+
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
