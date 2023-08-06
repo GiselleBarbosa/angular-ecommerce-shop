c217963f181cf7b2d6264be2606ac1cc3c56cc30
Giselle Barbosa <g88barbosa@gmail.com>
2023-08-06 19:51:22 -0300

    chore: added primeNg configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ThemeService } from './core/services/theme/theme.service';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,22 @@ import { ThemeService } from './core/services/theme/theme.service';
 export class AppComponent implements OnInit, OnDestroy {
   private subscription!: Subscription;
 
-  constructor(private themeService: ThemeService) {}
+  constructor(private themeService: ThemeService, private primengConfig: PrimeNGConfig) {}
 
   public ngOnInit(): void {
     this.subscription = this.themeService.theme$.subscribe(theme => {
       document.body.className = theme;
     });
+
+    // ripple - animations primeNg
+    this.primengConfig.ripple = true;
+
+    this.primengConfig.zIndex = {
+      modal: 1100, // dialog, sidebar
+      overlay: 1000, // dropdown, overlaypanel
+      menu: 1000, // overlay menus
+      tooltip: 1100, // tooltip
+    };
   }
 
   public ngOnDestroy(): void {
