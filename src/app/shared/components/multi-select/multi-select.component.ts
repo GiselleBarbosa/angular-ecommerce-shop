c95a6124b281e99a184e3a9a6f8ec9b361f28584
Giselle Barbosa <g88barbosa@gmail.com>
2023-08-25 07:35:52 -0300

    feat: multiSelect is now generic and can be reused

--- src/app/shared/components/multi-select/multi-select.component.ts
+++ src/app/shared/components/multi-select/multi-select.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MultiSelectModule } from 'primeng/multiselect';
 
-interface City {
-  name: string;
-  code: string;
-}
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
@@ -13,18 +9,8 @@ interface City {
   standalone: true,
   imports: [MultiSelectModule, FormsModule],
 })
-export class MultiSelectComponent implements OnInit {
-  public cities!: City[];
+export class MultiSelectComponent {
+  @Input() public categories!: any[];
 
-  public selectedCities!: City[];
-
-  public ngOnInit(): void {
-    this.cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' },
-    ];
-  }
+  public selectedCategories!: any[];
 }
