4a2d1f710daa0c37920177f89c0685af238b1861
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-14 00:09:32 -0300

    fix: bug resolved in translation

--- src/app/shared/internationalization/transloco-loader.ts
+++ src/app/shared/internationalization/transloco-loader.ts
@@ -1,7 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@ngneat/transloco';
 
-import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -10,6 +9,6 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   public getTranslation(lang: string): Observable<Translation> {
-    return this.http.get<Translation>(`${environment.baseUrl}/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
   }
 }
